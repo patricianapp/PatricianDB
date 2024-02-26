89f757509c15319547cd425ad8022f400b6bb7b0
Elias Jackson <ejthedj0@gmail.com>
2024-02-26 12:47:51 -0800

    Replace NestJS with graphql-mesh

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { ReleasesModule } from './releases/releases.module';
-
-@Module({
-  imports: [ConfigModule.forRoot(), ReleasesModule],
-  controllers: [],
-})
-export class AppModule {}
